5d431e7f79c51bd5aca17b8fc05771e42f5b7ddc
juliancastaeda <1277@holbertonschool.com>
2020-04-04 10:01:31 -0500

    uploaad file

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -16,17 +16,14 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if i != 15:
-            if (i % 3) == 0:
-                tmp_result.append("Fizz")
-            elif (i % 3) == 0 and (i % 5) == 0:
-                tmp_result.append("FizzBuzz")
-            elif (i % 5) == 0:
-                tmp_result.append("Buzz")
-            else:
-                tmp_result.append(str(i))
-        elif i == 15:
+        if (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
 
